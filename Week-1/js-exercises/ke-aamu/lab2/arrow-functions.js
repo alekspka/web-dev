aee9f6faf2bc084957def699095ff7f62533cf32
alekspka <aleksi.kaasalainen2@metropolia.fi>
2025-08-25 15:19:46 +0300

    updated exercises

---
+++ Week-1/js-exercises/ke-aamu/lab2/arrow-functions.js
@@ -0,0 +1,13 @@
+
+const sayHelloArrow = () => "Hello, World!";
+
+const doubleArrow = (x) => (x * 2);
+
+
+const addArrow =(x, y) => (x + y);
+
+module.exports = {
+    sayHelloArrow,
+    addArrow,
+    doubleArrow,
+};
