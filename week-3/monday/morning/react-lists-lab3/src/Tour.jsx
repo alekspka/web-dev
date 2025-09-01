efbc1723408dde5d802af75b2094cd4a72bb57e8
alekspka <aleksi.kaasalainen2@metropolia.fi>
2025-09-01 11:24:01 +0300

    monday morning

---
+++ week-3/monday/morning/react-lists-lab3/src/Tour.jsx
@@ -0,0 +1,21 @@
+const Tour = (props) => {
+        const { image, info, name, price } = props.tour;
+
+    return (
+        <article className = 'single-tour'>
+            <img src={image} alt= {name} />
+            <footer>
+                <div className ="tour-info">
+                    <h4>{name}</h4>
+                    <h4 className ='tour-price'>€{price}</h4>
+                </div>
+                <p>
+                    {`${info.substring(0, 200)}...`}
+                    <button>read more</button>
+                </p>
+            </footer>
+        </article>
+    );
+};
+
+export default Tour;
