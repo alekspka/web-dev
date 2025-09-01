0c6d89f493e306715f0a2e38345ef0559334fa8f
alekspka <aleksi.kaasalainen2@metropolia.fi>
2025-09-01 11:25:21 +0300

    .

---
+++ week-3/monday/morning/react-lists-lab1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Book from './Book'
+import booksData from './booksData';
+import './Book.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <div className="book-list">
+  {booksData.map(book => <Book key={book.id}  book={book} name="Matti"/>)} 
+      </div>
+    </div>
+  );
+}
+
+export default App;
