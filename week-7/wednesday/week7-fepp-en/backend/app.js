371c41c2e3769130944008e440bbf713b08be868
alekspka <aleksi.kaasalainen2@metropolia.fi>
2025-10-01 14:46:21 +0300

    Configure backend to serve frontend static files - single web service deployment

--- week-7/wednesday/week7-fepp-en/backend/app.js
+++ week-7/wednesday/week7-fepp-en/backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const path = require("path");
 const app = express();
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
@@ -12,11 +13,18 @@ app.use(cors())
 app.use(express.json());
 
 connectDB();
- 
 
+// API Routes
 app.use("/api/products", productRouter);
-
 app.use("/api/users", userRouter);
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Catch all handler: send back React's index.html file for any non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
